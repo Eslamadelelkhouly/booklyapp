6e6f16e69eeb6eee436d21946b4077d91dc15124
Eslamadelelkhouly <rodyerak@gmail.com>
2024-08-14 12:24:37 +0300

    add custom_app_bar

--- lib/features/home/presentation/views/widget/home_view_body.dart
+++ lib/features/home/presentation/views/widget/home_view_body.dart
@@ -1,10 +1,17 @@
+import 'package:booklyapp/core/utils/assets.dart';
+import 'package:booklyapp/features/home/presentation/views/widget/custom_app_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        const CustomAppBar(),
+      ],
+    );
   }
 }
