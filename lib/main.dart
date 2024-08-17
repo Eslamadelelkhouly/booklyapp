b8a34f30e36b4669f6701264b94456d759b99692
Eslamadelelkhouly <rodyerak@gmail.com>
2024-08-17 12:15:17 +0300

    refactor navigation add go router

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:booklyapp/constants.dart';
+import 'package:booklyapp/core/utils/app_router.dart';
 import 'package:booklyapp/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
@@ -14,7 +15,8 @@ class Booklyapp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
+    return MaterialApp.router(
+      routerConfig: AppRouter.router,
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: kPrimaryColor,
@@ -22,7 +24,6 @@ class Booklyapp extends StatelessWidget {
           ThemeData.dark().textTheme,
         ),
       ),
-      home: const SplashView(),
     );
   }
 }
