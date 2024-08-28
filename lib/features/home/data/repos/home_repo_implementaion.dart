cb8a1128f8b982f5869acd6088e3181926608619
Eslamadelelkhouly <rodyerak@gmail.com>
2024-08-28 12:57:09 +0300

    implement fetch similar books

--- lib/features/home/data/repos/home_repo_implementaion.dart
+++ lib/features/home/data/repos/home_repo_implementaion.dart
@@ -8,12 +8,13 @@ import 'package:dio/dio.dart';
 class HomeRepoImplementaion implements HomeRepo {
   final ApiService? apiService;
 
-  HomeRepoImplementaion({  this.apiService});
+  HomeRepoImplementaion({this.apiService});
   @override
   Future<Either<Failure, List<BookModel>>> fetchNewSetBooks() async {
     try {
       var data = await apiService!.get(
-          endPoint: 'volumes?q=programming&filter=ebooks&orderBy=newest');
+        endPoint: 'volumes?q=science&filter=ebooks',
+      );
       List<BookModel> books = [];
       for (var item in data['items']) {
         books.add(BookModel.fromJson(item));
@@ -30,9 +31,9 @@ class HomeRepoImplementaion implements HomeRepo {
 
   @override
   Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() async {
-      try {
-      var data = await apiService!.get(
-          endPoint: 'volumes?q=programming&filter=ebooks');
+    try {
+      var data = await apiService!
+          .get(endPoint: 'volumes?q=programming&filter=ebooks');
       List<BookModel> books = [];
       for (var item in data['items']) {
         books.add(BookModel.fromJson(item));
@@ -45,5 +46,25 @@ class HomeRepoImplementaion implements HomeRepo {
       }
       return left(ServerFailure(e.toString()));
     }
+  }
+  
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchSimilarBox({required String category}) async {
+    try {
+      var data = await apiService!
+          .get(endPoint: 'volumes?q=programming&filter=ebooks&Sorting=relevance');
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioException(e));
+      }
+      return left(ServerFailure(e.toString()));
+    }
+    throw UnimplementedError();
   }
 }
